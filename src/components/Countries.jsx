6dcec86a24e69a6fbdd93f1a4e647b18293272b2
maruf-ahmed <marufahamed1110@gmail.com>
2024-03-11 02:37:53 +0600

    added conditional styling and display spacific country name

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -4,6 +4,14 @@ import Country from "./Country";
 
 function Countries() {
   const [coutries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  let handleVisitedCountries = (country) => {
+    // console.log(country);
+    // console.log('added all visited country');
+    let newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+    console.log(visitedCountries);
+  };
   useEffect(() => {
     const fetchCountries = async () => {
       let res = await fetch("https://restcountries.com/v3.1/all");
@@ -15,9 +23,21 @@ function Countries() {
   return (
     <div className=" max-w-7xl mx-auto">
       <h1>Countries : {coutries.length}</h1>
+
+      <ul>
+        {visitedCountries.map((items) => {
+          console.log(items.name.common);
+          <li>{items.name.common}</li>;
+        })}
+      </ul>
+      <p>Visited Country : {visitedCountries.length}</p>
       <div className=" grid grid-cols-3">
         {coutries.map((items, index) => (
-          <Country coutries={items} key={index} />
+          <Country
+            coutries={items}
+            key={index}
+            handleVisitedCountries={handleVisitedCountries}
+          />
         ))}
       </div>
     </div>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 
-function Country({ coutries }) {
-  console.log(coutries);
+function Country({ coutries, handleVisitedCountries }) {
   const { name, flags, capital, cca3 } = coutries;
   let [visited, setVisited] = useState(false);
   const handleVisited = () => {
-    setVisited(true);
+    setVisited(!visited);
   };
-
+  //   console.log(handleVisitedCountries);
   return (
-    <div className=" bg-slate-200 m-4 p-6 rounded-xl">
+    <div
+      className={` m-4 p-6 rounded-xl ${
+        visited ? "bg-slate-200" : "bg-white"
+      } `}
+    >
       <img src={flags.png} alt="" />
       <h3 className=" font-bold text-xl">Name : {name.common}</h3>
       <p>Capital : {capital}</p>
@@ -21,6 +24,13 @@ function Country({ coutries }) {
         {visited ? "Visited" : "Visit"}
       </button>
       {visited ? "Already visited" : "I want to go this place"}
+      <br />
+      <button
+        onClick={() => handleVisitedCountries(coutries)}
+        className=" bg-red-500 py-2 px-4 rounded-xl text-white"
+      >
+        Mark Visited
+      </button>
     </div>
   );
 }
